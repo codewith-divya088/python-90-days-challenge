d4813413e8c36e6d4283387cfae60a5593279072
Divya singh <ds3144310@gmail.com>
2026-03-18 18:57:17 +0530

    Add string functions demonstration in Python

---
+++ day17_string_functions.py
@@ -0,0 +1,8 @@
+# Day 17: String Functions
+
+text = "hello world from python"
+
+print("Lowercase:", text.lower())
+print("Uppercase:", text.upper())
+print("Title Case:", text.title())
+print("Capitalized:", text.capitalize())
