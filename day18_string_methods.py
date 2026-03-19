b1dd7e5cbcccc828754471dbd0305cc918d32e06
Divya singh <ds3144310@gmail.com>
2026-03-19 19:30:52 +0530

    Implement string methods examples in day18_string_methods.py
    Added examples demonstrating various string methods in Python.

---
+++ day18_string_methods.py
@@ -0,0 +1,15 @@
+# Day 18: String Functions
+
+text = "Python123"
+
+print("Find:", text.find("Python"))
+print("Index:", text.index("123"))
+
+print("Is Alpha:", text.isalpha())
+print("Is Digit:", text.isdigit())
+print("Is Alnum:", text.isalnum())
+
+
+
+Day 18 - Learned string functions
+(find, index, isalpha, isdigit, isalnum)
