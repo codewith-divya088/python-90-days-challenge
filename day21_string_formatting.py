ff2d5bbb169e068a2a897f5a568e624f4d526ef9
Divya singh <ds3144310@gmail.com>
2026-03-22 19:11:04 +0530

    Day 21 - Learned string formatting

---
+++ day21_string_formatting.py
@@ -0,0 +1,10 @@
+# Day 21 - String Formatting
+
+name = "Divya"
+age = 20
+
+# Using f-string
+print(f"My name is {name} and I am {age} years old")
+
+# Using format()
+print("My name is {} and I am {} years old".format(name, age))
