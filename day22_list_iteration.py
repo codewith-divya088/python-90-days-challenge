94c37dc74e0a57f297de4c773e22f2dd7a2948bb
Divya singh <ds3144310@gmail.com>
2026-03-23 18:57:16 +0530

    Day 22 - List Iteration

---
+++ day22_list_iteration.py
@@ -0,0 +1,17 @@
+# Day 22 - List Iteration
+
+l=[10,20,30,40,50,60,70]
+t=len(l)
+print(t)
+
+
+
+
+
+# Another example
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print("I like", fruit)
+
