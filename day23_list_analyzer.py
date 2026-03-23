f8618cff22c1cab7564c0024a0e7a9275a504baa
Divya singh <ds3144310@gmail.com>
2026-03-23 18:59:43 +0530

    day23_list_analyzer.py

---
+++ day23_list_analyzer.py
@@ -0,0 +1,21 @@
+# Day 23 - List Analyzer Project
+
+numbers = [10, 25, 5, 40, 15]
+
+print("Numbers in the list:", numbers)
+
+# Find sum
+total = sum(numbers)
+print("Sum:", total)
+
+# Find maximum
+maximum = max(numbers)
+print("Maximum:", maximum)
+
+# Find minimum
+minimum = min(numbers)
+print("Minimum:", minimum)
+
+# Count elements
+count = len(numbers)
+print("Total elements:", count)
