bb77a6cb9b5ce639f628b1abb9d286bd948045a8
Divya singh <ds3144310@gmail.com>
2026-04-05 18:38:17 +0530

    Day 35 - Modules in Python

---
+++ my_module.py
@@ -0,0 +1,14 @@
+# My Module
+
+def greet(name):
+    return f"Hello, {name}"
+
+def add(a, b):
+    return a + b
+  # Day 35 - Using Module
+
+import my_module
+
+print(my_module.greet("Divya"))
+print("Sum:", my_module.add(5, 3))
+  
