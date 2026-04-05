0e1179043a1e30b39bdafce5bf48a1757016f8c4
Divya singh <ds3144310@gmail.com>
2026-04-05 18:48:08 +0530

    Implement basic arithmetic functions in calc_module

---
+++ calc_module.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+  ##second file 
+import calc_module
+
+print("Addition:", calc_module.add(5, 3))
+print("Subtraction:", calc_module.subtract(10, 4))
