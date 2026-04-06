16e6a317c97aa0a4ca0ff3db887da22a9d60f310
Divya singh <ds3144310@gmail.com>
2026-04-06 17:21:01 +0530

    Day 36 - Math, Random and Datetime modules in Python

---
+++ day36_modules_practice.py
@@ -0,0 +1,17 @@
+# Day 36 - Math, Random, Datetime Modules
+
+import math
+import random
+import datetime
+
+# Math module
+print("Square root of 25:", math.sqrt(25))
+print("Power 2^3:", math.pow(2, 3))
+
+# Random module
+print("Random number (1-10):", random.randint(1, 10))
+print("Random choice:", random.choice([1, 2, 3, 4]))
+
+# Datetime module
+now = datetime.datetime.now()
+print("Current date and time:", now)
