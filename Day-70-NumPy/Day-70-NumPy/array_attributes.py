2f5c450ea59eca08c478a1d2b1ca34721bad9774
Divya singh <ds3144310@gmail.com>
2026-06-25 15:49:19 +0530

    array_attributes.py

---
+++ Day-70-NumPy/Day-70-NumPy/array_attributes.py
@@ -0,0 +1,10 @@
+# Array Attributes
+
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("Shape:", numbers.shape)
+print("Dimensions:", numbers.ndim)
+print("Size:", numbers.size)
+print("Data Type:", numbers.dtype)
