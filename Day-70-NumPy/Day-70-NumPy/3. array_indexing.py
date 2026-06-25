6b20406e322eb2c586c3aa25e3f58cd9b7f39cc4
Divya singh <ds3144310@gmail.com>
2026-06-25 15:49:48 +0530

    3. array_indexing.py

---
+++ Day-70-NumPy/Day-70-NumPy/3. array_indexing.py
@@ -0,0 +1,9 @@
+# Array Indexing
+
+import numpy as np
+
+numbers = np.array([10, 20, 30, 40, 50])
+
+print("First Element:", numbers[0])
+print("Third Element:", numbers[2])
+print("Last Element:", numbers[-1])
