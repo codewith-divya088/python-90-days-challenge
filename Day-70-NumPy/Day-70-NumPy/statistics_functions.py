f8e3a05e5bac927a30d43eb486f9ea847efc5632
Divya singh <ds3144310@gmail.com>
2026-06-25 15:51:19 +0530

    Add statistical functions for marks using NumPy

---
+++ Day-70-NumPy/Day-70-NumPy/statistics_functions.py
@@ -0,0 +1,10 @@
+# Statistical Functions
+
+import numpy as np
+
+marks = np.array([85, 90, 78, 88, 95])
+
+print("Sum:", np.sum(marks))
+print("Average:", np.mean(marks))
+print("Highest:", np.max(marks))
+print("Lowest:", np.min(marks))
