0b5a3a125abeb985081a33f4633a3ea66686ba3d
Divya singh <ds3144310@gmail.com>
2026-06-27 20:15:41 +0530

    array_properties.py

---
+++ Day-71-NumPy-Array-Properties-and-Operations/array_properties.py
@@ -0,0 +1,15 @@
+# Day 71 - Array Properties
+
+import numpy as np
+
+# Creating an array
+numbers = np.array([10, 20, 30, 40, 50])
+
+# Displaying the array
+print("Array:", numbers)
+
+# Array properties
+print("Shape:", numbers.shape)
+print("Size:", numbers.size)
+print("Dimensions:", numbers.ndim)
+print("Data Type:", numbers.dtype)
